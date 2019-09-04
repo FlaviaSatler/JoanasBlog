bc1a9c851bf5b5128342026d7848a367388c3232
Flavia <flavia.satler@beetech.global>
2019-09-04 16:32:04 -0300

    feat(gatsby-config):add description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A blog about Joana`,
+    author: `@joana`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
